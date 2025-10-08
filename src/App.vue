99adf1dbbe50e61593f77eb95a685bd83734fb3a
Danikileitor <focusyi@hotmail.com>
2025-10-08 10:26:29 +0200

    start

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,9 @@ const digimonName = ref('')
 const digimonNameInput = ref('')
 const digimonList = ref([])
 const digimonImage = ref('')
+const priorEvolutions = ref([])
+const nextEvolutions = ref([])
+
 resetDigimon()
 
 // Fetch Digimon desde digi-api cuando el input pierde el foco
@@ -22,6 +25,8 @@ async function fetchDigimon() {
     if (data.content && data.content.length) {
       digimonList.value = data.content
       digimonImage.value = data.content[0].image
+      priorEvolutions.value = []
+      nextEvolutions.value = []
     } else {
       resetDigimon()
     }
@@ -37,13 +42,71 @@ function selectDigimon(id) {
   if (digimonList.value.length) {
     digimonName.value = digimonList.value[0].name
     digimonImage.value = digimonList.value[0].image
+    priorEvolutions.value = []
+    nextEvolutions.value = []
   }
+}
+
+async function showPriorEvolutions(digimon) {
+  if (!digimon.priorEvolutions || !digimon.priorEvolutions.length) {
+    priorEvolutions.value = []
+    return
+  }
+  const evolutions = []
+  for (const evo of digimon.priorEvolutions) {
+    const res = await fetch(`https://digi-api.com/api/v1/digimon/${evo.id}`)
+    const data = await res.json()
+    if (data && data.id) {
+      evolutions.push({
+        ...data,
+        image: data.images?.[0]?.href || digimonDefaultImage
+      })
+    }
+  }
+  priorEvolutions.value = evolutions
+}
+
+async function showNextEvolutions(digimon) {
+  if (!digimon.nextEvolutions || !digimon.nextEvolutions.length) {
+    nextEvolutions.value = []
+    return
+  }
+  const evolutions = []
+  for (const evo of digimon.nextEvolutions) {
+    const res = await fetch(`https://digi-api.com/api/v1/digimon/${evo.id}`)
+    const data = await res.json()
+    if (data && data.id) {
+      evolutions.push({
+        ...data,
+        image: data.images?.[0]?.href || digimonDefaultImage
+      })
+    }
+  }
+  nextEvolutions.value = evolutions
 }
 
 function resetDigimon() {
   digimonList.value = []
   digimonImage.value = digimonDefaultImage
   digimonName.value = 'Digimon'
+  priorEvolutions.value = []
+  nextEvolutions.value = []
+}
+
+// Funciones recursivas para evoluciones
+async function handleLeftClick(digimon) {
+  // Consulta por ID para obtener priorEvolutions
+  const res = await fetch(`https://digi-api.com/api/v1/digimon/${digimon.id}`)
+  const data = await res.json()
+  await showPriorEvolutions(data)
+  nextEvolutions.value = []
+}
+async function handleRightClick(digimon) {
+  // Consulta por ID para obtener nextEvolutions
+  const res = await fetch(`https://digi-api.com/api/v1/digimon/${digimon.id}`)
+  const data = await res.json()
+  await showNextEvolutions(data)
+  priorEvolutions.value = []
 }
 </script>
 
@@ -56,7 +119,21 @@ function resetDigimon() {
   </header>
 
   <main>
-    <div v-if="digimonList.length">
+    <div v-if="digimonList.length === 1">
+      <div class="evolution-row">
+        <div class="evolution-col" v-if="priorEvolutions.length">
+          <DigimonItem v-for="digimon in priorEvolutions" :key="digimon.id" :name="digimon.name" :image="digimon.image"
+            @left-click="() => handleLeftClick(digimon)" @right-click="() => handleRightClick(digimon)" />
+        </div>
+        <DigimonItem :name="digimonList[0].name" :image="digimonList[0].image"
+          @left-click="() => handleLeftClick(digimonList[0])" @right-click="() => handleRightClick(digimonList[0])" />
+        <div class="evolution-col" v-if="nextEvolutions.length">
+          <DigimonItem v-for="digimon in nextEvolutions" :key="digimon.id" :name="digimon.name" :image="digimon.image"
+            @left-click="() => handleLeftClick(digimon)" @right-click="() => handleRightClick(digimon)" />
+        </div>
+      </div>
+    </div>
+    <div v-else-if="digimonList.length">
       <DigimonItem v-for="digimon in digimonList" :key="digimon.id" :name="digimon.name" :image="digimon.image"
         @click="selectDigimon(digimon.id)" />
     </div>
@@ -99,6 +176,20 @@ h1 {
 .digimon-input:focus {
   border-color: #2d6fa3;
   box-shadow: 0 0 0 2px #b3e0ff;
+}
+
+.evolution-row {
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: center;
+  gap: 24px;
+}
+
+.evolution-col {
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
 }
 
 @media (min-width: 1024px) {
